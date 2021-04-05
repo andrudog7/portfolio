4bd9adc311b5c155034afc476c6cc9d562085ed0
--add <andrewksmoker@gmail.com>
2021-04-05 14:55:31 -0400

    updating blog post view

--- src/Components/BlogPostCard.js
+++ src/Components/BlogPostCard.js
@@ -3,23 +3,19 @@ import { Grid, Card, Image, Icon} from 'semantic-ui-react';
 
 class BlogPostCard extends React.Component {
   render() {
-    let shortenText = (text,startingPoint ,maxLength) => {
-        return text.length > maxLength?
-           text.slice(startingPoint, maxLength):
-           text
-       }
     return (
       <Grid.Column id="blog-column">
           <Card centered color="gray" id="blog-card" href={this.props.post.link}>
             <Image src={this.props.post.thumbnail} wrapped ui={false} size="medium" />
             <Card.Content>
                 <Card.Header>{this.props.post.title}</Card.Header>
-                <Card.Description>{shortenText(this.props.post.content, 110, 300)}</Card.Description>
+                <Card.Description>{this.props.content}</Card.Description>
                 </Card.Content>
             <Card.Content extra>
               <Card.Meta>{this.props.post.author} - {this.props.post.pubDate}</Card.Meta>
             </Card.Content>
         </Card>
+        <br></br>
       </Grid.Column>
     );
   }
--- src/Containers/BlogPostsContainer.js
+++ src/Containers/BlogPostsContainer.js
@@ -4,21 +4,36 @@ import BlogPostCard from '../Components/BlogPostCard'
 
 class BlogPostsContainer extends React.Component {
     state = {
-        posts: []
+        posts: [],
+        content: []
     }
     componentDidMount() {
         fetch("https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@andrewksmoker")
         .then((res) => res.json())
         .then((data) => {
+            let shortenText = (text,startingPoint ,maxLength) => {
+                return text.length > maxLength?
+                text.slice(startingPoint, maxLength):
+                text
+            }
+            let content = []
+            content.push(shortenText(data.items[0].content, 110, 300))
+            content.push(shortenText(data.items[1].content, 110, 300))
+            content.push(shortenText(data.items[2].content, 178, 300))
+            content.push(shortenText(data.items[3].content, 109, 300))
+            content.push(shortenText(data.items[4].content, 108, 300))
+            content.push(shortenText(data.items[5].content, 160, 300))
+            content.push(shortenText(data.items[6].content, 109, 300))
             this.setState({
-                posts: data.items
+                posts: data.items,
+                content: content
             })
         })
     }
 
   render() {
-      let showBlogPosts = this.state.posts.map(p => <BlogPostCard post={p}></BlogPostCard>)
-     return (
+    let showBlogPosts = this.state.posts.map((p, index) => <BlogPostCard post={p} content={this.state.content[index]}></BlogPostCard>)
+    return (
         <Grid centered container>
             <Grid.Row columns={3}>
               {showBlogPosts}  
