6e5078a00b0ad998cb7f4e147bbae43fe8f93e9b
--add <andrewksmoker@gmail.com>
2021-04-05 21:45:56 -0400

    updating language in bio

--- src/Components/Bio.js
+++ src/Components/Bio.js
@@ -19,9 +19,9 @@ class Bio extends React.Component {
                 <Image src={Profile} circular floated="left" size="medium"></Image>
                 {/* <figcaption>Nice to meet you!</figcaption> */}
             </figure>
-            <p>I am a full-stack web developer, specialized in Ruby on Rails, React and Javascript.  I am open to new opportunities where I can use my skills in creating user-friendly applications.  I'm excited to join a team who's mission is to make things a little easier for their clients.  We all could use a little easy these days!</p>
+            <p>I am a full-stack web developer, specialized in Ruby on Rails, React and Javascript.  I am open to new opportunities where I can use my skills in creating user-friendly applications.  I'm excited to join a team whose mission is to make things a little easier for their clients.  We all could use a little easy these days!</p>
             <p>I write organized code with an emphasis on clarity and usability, making even large-scale applications easy to manage.   
-            My attention-to-detail, ability to debug issues and dedication to producing quality apps make me a valuable asset to any company.
+            My attention to detail, ability to debug issues and dedication to producing quality apps make me a valuable asset to any company.
             </p>
             <p>I am a marathon runner who enjoys traveling and experiencing other cultures.  I enjoy coding because it allows me to solve logical problems in a creative way.</p>
             <br></br>
