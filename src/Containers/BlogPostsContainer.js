9e95ec1e5a37d22f08bb1406d89d222e4547f2d9
--add <andrewksmoker@gmail.com>
2021-05-27 17:10:51 -0400

    adding new blog post to portfolio

--- src/Containers/BlogPostsContainer.js
+++ src/Containers/BlogPostsContainer.js
@@ -17,16 +17,17 @@ class BlogPostsContainer extends React.Component {
                 text
             }
             let content = []
-            content.push(shortenText(data.items[0].content, 110, 337))
-            content.push(shortenText(data.items[1].content, 4, 297))
-            content.push(shortenText(data.items[2].content, 110, 358))
-            content.push(shortenText(data.items[3].content, 110, 349))
-            content.push(shortenText(data.items[4].content, 110, 302))
-            content.push(shortenText(data.items[5].content, 109, 299))
-            content.push(shortenText(data.items[6].content, 109, 437))
-            content.push(shortenText(data.items[7].content, 108, 443))
-            content.push(shortenText(data.items[8].content, 160, 462))
-            content.push(shortenText(data.items[9].content, 109, 299))
+            content.push(shortenText(data.items[0].content, 110, 360))
+            content.push(shortenText(data.items[1].content, 110, 337))
+            content.push(shortenText(data.items[2].content, 4, 297))
+            content.push(shortenText(data.items[3].content, 110, 358))
+            content.push(shortenText(data.items[4].content, 110, 349))
+            content.push(shortenText(data.items[5].content, 110, 302))
+            content.push(shortenText(data.items[6].content, 109, 299))
+            content.push(shortenText(data.items[7].content, 109, 437))
+            content.push(shortenText(data.items[8].content, 108, 443))
+            content.push(shortenText(data.items[9].content, 160, 462))
+            //content.push(shortenText(data.items[10].content, 109, 299))
             this.setState({
                 posts: data.items,
                 content: content
