d3e1ab11f8cefcd36e5d7954b049508bd0290993
--add <andrewksmoker@gmail.com>
2021-06-29 13:04:56 -0400

    Added latest blog post

--- src/Containers/BlogPostsContainer.js
+++ src/Containers/BlogPostsContainer.js
@@ -17,21 +17,22 @@ class BlogPostsContainer extends React.Component {
                 text
             }
             let content = []
-            content.push(shortenText(data.items[0].content, 109, 365))
-            content.push(shortenText(data.items[1].content, 110, 490))
-            content.push(shortenText(data.items[2].content, 110, 460))
-            content.push(shortenText(data.items[3].content, 110, 343))
-            content.push(shortenText(data.items[4].content, 110, 360))
-            content.push(shortenText(data.items[5].content, 110, 337))
-            content.push(shortenText(data.items[6].content, 4, 297))
-            content.push(shortenText(data.items[7].content, 110, 358))
-            content.push(shortenText(data.items[8].content, 110, 349))
-            content.push(shortenText(data.items[9].content, 110, 302))
-            //content.push(shortenText(data.items[10].content, 109, 299))
-            //content.push(shortenText(data.items[11].content, 109, 437))
-            //content.push(shortenText(data.items[12].content, 108, 443))
-            //content.push(shortenText(data.items[13].content, 160, 462))
-            //content.push(shortenText(data.items[14].content, 109, 299))
+            content.push(shortenText(data.items[0].content, 110, 525))
+            content.push(shortenText(data.items[1].content, 109, 365))
+            content.push(shortenText(data.items[2].content, 110, 490))
+            content.push(shortenText(data.items[3].content, 110, 460))
+            content.push(shortenText(data.items[4].content, 110, 343))
+            content.push(shortenText(data.items[5].content, 110, 360))
+            content.push(shortenText(data.items[6].content, 110, 337))
+            content.push(shortenText(data.items[7].content, 4, 297))
+            content.push(shortenText(data.items[8].content, 110, 358))
+            content.push(shortenText(data.items[9].content, 110, 349))
+            //content.push(shortenText(data.items[10].content, 110, 302))
+            //content.push(shortenText(data.items[11].content, 109, 299))
+            //content.push(shortenText(data.items[12].content, 109, 437))
+            //content.push(shortenText(data.items[13].content, 108, 443))
+            //content.push(shortenText(data.items[14].content, 160, 462))
+            //content.push(shortenText(data.items[15].content, 109, 299))
             this.setState({
                 posts: data.items,
                 content: content
